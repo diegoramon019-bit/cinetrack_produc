527ea2b7de2710748c7408d138324d28b3e6cfeb
diegoramon019-bit <diegoramon.019@gmail.com>
2025-12-06 16:51:28 -0300

    Fix MySQL connection for production

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,4 @@
-// backend/db.js
-import mysql from "mysql2/promise"; // consultas.
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -7,16 +6,16 @@ let db;
 
 try {
   db = await mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "",
-    database: process.env.DB_NAME || "cine_track",
-    port: 3306, // estándar
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT)
   });
 
-  console.log("LEST FUCKING GOOOOOOO Conectado correctamente a la base de datos:", process.env.DB_NAME);
+  console.log("🔥 Conectado correctamente a la base de datos:", process.env.DB_NAME);
 } catch (err) {
-  console.error(" Error al conectar con MySQL:", err.message);
+  console.error("❌ Error al conectar con MySQL:", err.message);
 }
 
 export default db;
